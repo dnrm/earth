6668b5fde57237f5b57bf08a37f96c78f15e532e
Daniel Medina <daniel@medina.com>
2022-01-13 21:18:52 -0600

    :sparkles: First day and I made the top text, beginning of navbar, and quote

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        dark: "#222222",
+        leaf: "#36452C"
+      },
+      fontFamily: {
+        "space-grotesk": "'Space Grotesk', sans-serif;",
+        "raleway": "'Raleway', sans-serif;"
+      }
+    },
+  },
+  plugins: [],
+}
